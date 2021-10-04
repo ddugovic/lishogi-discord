2e9842a07603102570f6cb9f0e01c66cef62c88b
Daniel Dugovic <dandydand@gmail.com>
2021-10-04 15:20:32 -0500

    Update privacy.js

--- commands/privacy.js
+++ commands/privacy.js
@@ -1,6 +1,6 @@
 // View privacy policy
-function privacy(bot, msg, suffix) {
-    msg.channel.send('Contact Toadofsky on Tadpole Pond if you need your data removed.')
+function privacy(bot, msg) {
+    msg.channel.send('This bot only stores your Lichess username and favorite game mode. Contact Toadofsky on Tadpole Pond if you need your data removed and cannot figure out how to use the deleteuser command.')
 }
 
 module.exports = privacy;
