b0242d38bf58be2e2c54dd41177f9262cdb40470
Daniel Dugovic <dandydand@gmail.com>
2022-02-05 12:27:38 -0600

    Create slash commands

---
+++ deploy-commands.js
@@ -0,0 +1,25 @@
+const config = require('./config.json');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const commands = [
+    new SlashCommandBuilder().setName('arena').setDescription("Find an upcoming or recent arena created by lichess (or a user)"),
+    new SlashCommandBuilder().setName('deleteuser').setDescription("Deletes your lichess username from the bot's database"),
+    new SlashCommandBuilder().setName('playing').setDescription("Shares your (or a user's) ongoing game"),
+    new SlashCommandBuilder().setName('profile').setDescription("Displays your (or a user's) profile"),
+    new SlashCommandBuilder().setName('puzzle').setDescription("Displays today's puzzle"),
+    new SlashCommandBuilder().setName('recent').setDescription("Shares your most recent game"),
+    new SlashCommandBuilder().setName('setgamemode').setDescription("Sets your favorite game (or puzzle) mode"),
+    new SlashCommandBuilder().setName('setuser').setDescription("Sets your lichess username"),
+    new SlashCommandBuilder().setName('tv').setDescription("Shares the featured game"),
+    new SlashCommandBuilder().setName('whoami').setDescription("Returns your lichess username"),
+    new SlashCommandBuilder().setName('privacy').setDescription("View privacy policy")
+]
+    .map(command => command.toJSON());
+
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const rest = new REST({ version: '9' }).setToken(config.token);
+
+rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: commands })
+    .then(() => console.log('Successfully registered application slash commands.'))
+    .catch(console.error);
+
