0fb8bb7bb4e6438d34d8fa212348cf2250374ad1
Daniel Dugovic <dandydand@gmail.com>
2022-02-11 13:03:56 -0600

    Implement deleteUser interaction

--- commands/deleteUser.js
+++ commands/deleteUser.js
@@ -1,24 +1,20 @@
 const User = require('../models/User');
 
-function process(bot, msg, username) {
-    var authorId = msg.author.id;
-    User.findOne({ userId: authorId }, (err, result) => {
-        if (err) {
-            console.log(err);
-            msg.channel.send('An error occured in your request.');
-        }
-        if (result) {
-            User.deleteOne({ userId: authorId }, (err) => {
-                msg.channel.send(`User deleted: ${msg.author.username}`);
-            });
-        } else {
-            msg.channel.send('No user found!');
-        }
-    });
+async function deleteUser(author, username) {
+    if (await User.findByIdAndDelete(author.id).exec()) {
+        return `User deleted: ${author.username}`;
+    }
+    else {
+        return 'An error occured in your request.';
+    }
 }
 
-function reply(interaction) {
-    return message;
+function process(bot, msg, suffix) {
+    deleteUser(msg.author).then(message => msg.channel.send(message));
+}
+
+async function reply(interaction) {
+    return deleteUser(interaction.user);
 }
 
 module.exports = {process, reply};
