0039f7881be9f7b7ef4ac927314b9d7e17480159
Daniel Dugovic <dandydand@gmail.com>
2022-02-11 19:36:29 -0600

    Add command parameters

--- commands/tv.js
+++ commands/tv.js
@@ -1,40 +1,40 @@
 const axios = require('axios');
 const User = require('../models/User');
 
-async function tv(author, favoriteMode) {
+async function tv(author, mode) {
     const user = await User.findById(author.id).exec();
-    if (!favoriteMode) {
+    if (!mode) {
         if (!user) {
             return 'You need to set your lishogi username with setuser!';
         } else if (!user.favoriteMode) {
             return 'You need to set your favorite gamemode with setgamemode!';
         }
-	favoriteMode = user.favoriteMode;
+	mode = user.favoriteMode;
     }
     return axios.get('https://lishogi.org/tv/channels')
-        .then(response => formatTv(response.data, favoriteMode))
+        .then(response => formatTv(response.data, mode))
         .catch((err) => {
-            console.log(`Error in tv(${author.username}, ${favoriteMode}): \
+            console.log(`Error in tv(${author.username}, ${mode}): \
                 ${suffix} ${err.response.status} ${err.response.statusText}`);
             return `An error occurred handling your request: \
                 ${err.response.status} ${err.response.statusText}`;
         });
 }
 
-function formatTv(data, favoriteMode) {
+function formatTv(data, mode) {
     for (var channel in data) {
-        if (channel.toLowerCase() == favoriteMode)
+        if (channel.toLowerCase() == mode)
             return 'https://lishogi.org/' + data[channel].gameId;
     }
-    return `No channel of mode ${favoriteMode} found!`;
+    return `No channel of mode ${mode} found!`;
 }
 
-function process(bot, msg, favoriteMode) {
-    tv(msg.author, favoriteMode).then(message => msg.channel.send(message));
+function process(bot, msg, mode) {
+    tv(msg.author, mode).then(message => msg.channel.send(message));
 }
 
 async function reply(interaction) {
-    return tv(interaction.user, interaction.options.favoriteMode);
+    return tv(interaction.user, interaction.options.mode);
 }
 
 module.exports = {process, reply};
