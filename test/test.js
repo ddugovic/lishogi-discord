6401d2d15e4b8d70ad152e30e1977492059e6b94
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-05-27 15:13:58 -0500

    Fix unit test

--- test/test.js
+++ test/test.js
@@ -2,11 +2,20 @@ var assert = require('assert');
 var format = require('../lib/format-seconds');
 describe('Commands', function() {
   describe('formatSeconds()', function() {
-    it('should format 1 minute', function() {
-      assert.equal("1 minute", format.formatSeconds(60));
+    it('should format 0 seconds', function() {
+      assert.equal("0 seconds", format.formatSeconds(0));
     });
-    it('should format 2 minutes', function() {
-      assert.equal("2 minutes", format.formatSeconds(120));
+    it('should format 1 second', function() {
+      assert.equal("1 second", format.formatSeconds(1));
+    });
+    it('should format 2 seconds', function() {
+      assert.equal("2 seconds", format.formatSeconds(2));
+    });
+    it('should format 1 minute, 1 second', function() {
+      assert.equal("1 minute, 1 second", format.formatSeconds(61));
+    });
+    it('should format 2 minutes, 2 seconds', function() {
+      assert.equal("2 minutes, 2 seconds", format.formatSeconds(122));
     });
     it('should format 1 hour', function() {
       assert.equal("1 hour", format.formatSeconds(3600));
@@ -19,9 +28,6 @@ describe('Commands', function() {
     });
     it('should format 2 days', function() {
       assert.equal("2 days", format.formatSeconds(172800));
-    });
-    it('should format 1 day, 1 hour, 1 minute', function() {
-      assert.equal("1 day, 1 hour, 1 minute", format.formatSeconds(90061));
     });
   });
 });
