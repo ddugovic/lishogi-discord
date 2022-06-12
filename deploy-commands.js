b1f124979053405622dcb2a2b7a2e689a06a1cf8
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-12 12:02:11 -0500

    Define game mode choices

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,17 +1,56 @@
 const config = require('./config.json');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const channels = [
+    { name: 'Top Rated', value: 'Top Rated' },
+    { name: 'All Chess', value: 'All Chess' },
+    { name: 'All Draughts', value: 'All Draughts' },
+    { name: 'All Shogi', value: 'All Shogi' },
+    { name: 'All Xiangqi', value: 'All Xiangqi' },
+    { name: 'Computer', value: 'Computer' }
+];
+const modes = [
+    { name: 'Antichess', value: 'antichess' },
+    { name: 'Antidraughts', value: 'antidraughts' },
+    { name: 'Atomic', value: 'atomic' },
+    { name: 'Blitz', value: 'blitz' },
+    { name: 'Brazilian', value: 'brazilian' },
+    { name: 'Breakthrough', value: 'breakthrough' },
+    { name: 'Bullet', value: 'bullet' },
+    { name: 'Chess960', value: 'chess960' },
+    { name: 'Classical', value: 'classical' },
+    { name: 'Crazyhouse', value: 'crazyhouse' },
+    { name: 'Five-check', value: 'fiveCheck' },
+    { name: 'Frisian', value: 'frisian' },
+    { name: 'Frysk!', value: 'frysk' },
+    { name: 'Horde', value: 'Horde' },
+    { name: 'Mini Shogi', value: 'minishogi' },
+    { name: 'Mini Xiangqi', value: 'minixiangqi' },
+    { name: 'King of the Hill', value: 'kingOfTheHill' },
+    { name: 'Othello', value: 'othello' },
+    { name: 'Oware', value: 'oware' },
+    { name: 'Pool', value: 'pool' },
+    { name: 'Racing Kings', value: 'racingKings' },
+    { name: 'Rapid', value: 'rapid' },
+    { name: 'Russian', value: 'russian' },
+    { name: 'Shogi', value: 'shogi' },
+    { name: 'Three-check', value: 'threeCheck' },
+    { name: 'UltraBullet', value: 'ultraBullet' },
+    { name: 'Xiangqi', value: 'xiangqi' }
+];
+// PlayStrategy does not have puzzle ratings yet
+const ratings = modes;
 const commands = [
-    new SlashCommandBuilder().setName('arena').setDescription("Find an upcoming or recent arena").addStringOption(option => option.setName('mode').setDescription('Enter a game mode')),
+    new SlashCommandBuilder().setName('arena').setDescription("Find an upcoming or recent arena").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...modes)),
     new SlashCommandBuilder().setName('deleteuser').setDescription("Deletes your playstrategy username from the bot's database"),
-    new SlashCommandBuilder().setName('leaderboard').setDescription("Displays the leaderboard top player").addStringOption(option => option.setName('mode').setDescription('Enter a game mode')),
+    new SlashCommandBuilder().setName('leaderboard').setDescription("Displays the leaderboard top player").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...modes)),
     new SlashCommandBuilder().setName('playing').setDescription("Shares your (or a user's) current game URL").addStringOption(option => option.setName('username').setDescription('Enter playstrategy player username')),
     new SlashCommandBuilder().setName('gif').setDescription("Shares your (or a user's) current game as a GIF").addStringOption(option => option.setName('username').setDescription('Enter playstrategy player username')),
     new SlashCommandBuilder().setName('privacy').setDescription("View privacy policy"),
     new SlashCommandBuilder().setName('profile').setDescription("Displays your (or a user's) profile").addStringOption(option => option.setName('username').setDescription('Enter playstrategy player username')),
     new SlashCommandBuilder().setName('puzzle').setDescription("Displays today's puzzle"),
-    new SlashCommandBuilder().setName('setgamemode').setDescription("Sets your favorite game (or puzzle) mode").addStringOption(option => option.setName('mode').setDescription('Enter your favorite game (or puzzle) mode')),
+    new SlashCommandBuilder().setName('setgamemode').setDescription("Sets your favorite game (or puzzle) mode").addStringOption(option => option.setName('mode').setDescription('Enter your favorite game (or puzzle) mode').addChoices(...ratings)),
     new SlashCommandBuilder().setName('setuser').setDescription("Sets your playstrategy username").addStringOption(option => option.setName('username').setDescription('Enter your playstrategy username')),
-    new SlashCommandBuilder().setName('tv').setDescription("Shares the featured game").addStringOption(option => option.setName('mode').setDescription('Enter your favorite game (or puzzle) mode')),
+    new SlashCommandBuilder().setName('tv').setDescription("Shares the featured game").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...channels)),
     new SlashCommandBuilder().setName('help').setDescription("Sends a list of available commands")
 ]
     .map(command => command.toJSON());
