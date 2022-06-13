581438becb5817eaff68568300de63ab5bfbf671
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-13 12:54:34 -0500

    Capitalize rating category

--- commands/profile.js
+++ commands/profile.js
@@ -76,9 +76,9 @@ function formatRatings(ratings) {
     for (var i = 0; i < modes.length; i++) {
         // puzzles are classic and untimed
         const category = modes[i][0].split('.');
-        const game = category[1] == 'puzzle' ? 'classic' : category[1];
-        const speed = category[1] == 'puzzle' ? 'puzzle' : category[2];
-        const lexicon = `${category[0]} ${title(game)}`;
+        const game = title(category[1] == 'puzzle' ? 'classic' : category[1]);
+        const speed = title(category[1] == 'puzzle' ? 'puzzle' : category[2]);
+        const lexicon = `${category[0]} ${game}`;
         const rating = modes[i][1];
         if (ratings[lexicon]) {
             ratings[lexicon].push(`${speed}: ${rating.r.toFixed(0)} ± ${(2 * rating.rd).toFixed(0)}`);
