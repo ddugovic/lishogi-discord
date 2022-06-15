2a74b5dba763729052676b2bf54dfd25d3b1dfda
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-15 12:28:05 -0500

    Add simul command

--- deploy-commands.js
+++ deploy-commands.js
@@ -29,19 +29,20 @@ const ratings = [
 ];
 const commands = [
     new SlashCommandBuilder().setName('arena').setDescription("Find an upcoming or recent arena").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...modes)),
-    new SlashCommandBuilder().setName('deleteuser').setDescription("Deletes your lishogi username from the bot's database"),
+    new SlashCommandBuilder().setName('deleteuser').setDescription("Delete your lishogi username from the bot's database"),
     new SlashCommandBuilder().setName('eval').setDescription("Get the cached evaluation of a position, if available").addStringOption(option => option.setName('sfen').setDescription('SFEN (Shogi Forsyth-Edwards Notation)')),
-    new SlashCommandBuilder().setName('leaderboard').setDescription("Displays the leaderboard top player").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...modes)),
-    new SlashCommandBuilder().setName('playing').setDescription("Shares your (or a user's) current game URL").addStringOption(option => option.setName('username').setDescription('Enter lishogi player username')),
-    new SlashCommandBuilder().setName('gif').setDescription("Shares your (or a user's) current game as a GIF").addStringOption(option => option.setName('username').setDescription('Enter lishogi player username')),
+    new SlashCommandBuilder().setName('leaderboard').setDescription("Display the leaderboard top player").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...modes)),
+    new SlashCommandBuilder().setName('playing').setDescription("Share your (or a user's) current game URL").addStringOption(option => option.setName('username').setDescription('Enter lishogi player username')),
+    new SlashCommandBuilder().setName('gif').setDescription("Share your (or a user's) current game as a GIF").addStringOption(option => option.setName('username').setDescription('Enter lishogi player username')),
     new SlashCommandBuilder().setName('privacy').setDescription("View privacy policy"),
-    new SlashCommandBuilder().setName('profile').setDescription("Displays your (or a user's) profile").addStringOption(option => option.setName('username').setDescription('Enter lishogi player username')),
-    new SlashCommandBuilder().setName('puzzle').setDescription("Displays today's puzzle"),
-    new SlashCommandBuilder().setName('setgamemode').setDescription("Sets your favorite game (or puzzle) mode").addStringOption(option => option.setName('mode').setDescription('Enter your favorite game (or puzzle) mode').addChoices(...ratings)),
-    new SlashCommandBuilder().setName('setuser').setDescription("Sets your lishogi username").addStringOption(option => option.setName('username').setDescription('Enter your lishogi username')),
-    new SlashCommandBuilder().setName('streamers').setDescription("Displays live streamers"),
-    new SlashCommandBuilder().setName('tv').setDescription("Shares the featured game").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...channels)),
-    new SlashCommandBuilder().setName('help').setDescription("Sends a list of available commands")
+    new SlashCommandBuilder().setName('profile').setDescription("Display your (or a user's) profile").addStringOption(option => option.setName('username').setDescription('Enter lishogi player username')),
+    new SlashCommandBuilder().setName('puzzle').setDescription("Display today's puzzle"),
+    new SlashCommandBuilder().setName('setgamemode').setDescription("Set your favorite game (or puzzle) mode").addStringOption(option => option.setName('mode').setDescription('Enter your favorite game (or puzzle) mode').addChoices(...ratings)),
+    new SlashCommandBuilder().setName('setuser').setDescription("Set your lishogi username").addStringOption(option => option.setName('username').setDescription('Enter your lishogi username')),
+    new SlashCommandBuilder().setName('simul').setDescription("Display a recently finished, ongoing, or upcoming simultanous exhibition"),
+    new SlashCommandBuilder().setName('streamers').setDescription("Display live streamers"),
+    new SlashCommandBuilder().setName('tv').setDescription("Share the featured game").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...channels)),
+    new SlashCommandBuilder().setName('help').setDescription("Display a list of available commands")
 ]
     .map(command => command.toJSON());
 
