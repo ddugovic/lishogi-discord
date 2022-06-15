9b2eb30f2cfb2fa2423d7c9ee1b4cc76ce34124b
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-15 15:52:00 -0500

    Code cleanup

--- commands/tv.js
+++ commands/tv.js
@@ -6,7 +6,7 @@ async function tv(author, mode) {
         const user = await User.findById(author.id).exec();
         mode = (user && user.favoriteMode) ? user.favoriteMode : 'top rated';
     }
-    url = 'https://playstrategy.org/tv/channels';
+    const url = 'https://playstrategy.org/tv/channels';
     return axios.get(url, { headers: { Accept: 'application/vnd.playstrategy.v3+json' } })
         .then(response => formatTv(response.data, mode))
         .catch((err) => {
@@ -18,9 +18,9 @@ async function tv(author, mode) {
 }
 
 function formatTv(data, mode) {
-    for (var channel in data) {
+    for (channel in data) {
         if (channel.toLowerCase() == mode.toLowerCase())
-            return 'https://playstrategy.org/' + data[channel].gameId;
+            return `https://playstrategy.org/${data[channel].gameId}`;
     }
     return `No channel of mode ${mode} found!`;
 }
