5f39d2ed74a974c8dfcffa366efceb751f418794
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-15 15:52:00 -0500

    Code cleanup

--- commands/tv.js
+++ commands/tv.js
@@ -6,7 +6,7 @@ async function tv(author, mode) {
         const user = await User.findById(author.id).exec();
         mode = (user && user.favoriteMode) ? user.favoriteMode : 'blitz';
     }
-    url = 'https://lishogi.org/tv/channels';
+    const url = 'https://lishogi.org/tv/channels';
     return axios.get(url, { headers: { Accept: 'application/vnd.lishogi.v3+json' } })
         .then(response => formatTv(response.data, mode))
         .catch((err) => {
@@ -18,9 +18,9 @@ async function tv(author, mode) {
 }
 
 function formatTv(data, mode) {
-    for (var channel in data) {
+    for (channel in data) {
         if (channel.toLowerCase() == mode.toLowerCase())
-            return 'https://lishogi.org/' + data[channel].gameId;
+            return `https://lishogi.org/${data[channel].gameId}`;
     }
     return `No channel of mode ${mode} found!`;
 }
