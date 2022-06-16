f36d87e1f90e589153ca0f3dd646dcf9f400ef00
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-16 02:30:59 -0500

    Add profile patron

--- commands/profile.js
+++ commands/profile.js
@@ -41,18 +41,20 @@ function formatProfile(data, favoriteMode) {
     if (data.title)
         playerName = `${data.title} ${playerName}`;
 
-    var colorEmoji;
-    if (data.playing) {
-        colorEmoji = data.playing.includes('white') ? '⚪' : '⚫';
-    }
-    var status = (!data.online ? '🔴 Offline' : (colorEmoji ? colorEmoji + ' Playing' : '📶 Online'));
+    const link = data.playing ?? data.url;
+    var status = '';
     if (data.streaming)
-        status = '📡 Streaming  ' + status;
+        status = '📡 Streaming';
+    if (data.playing)
+        status += data.playing.includes('white') ? '  ⚪ Playing' : '  ⚫ Playing';
+    else if (!status)
+        status = (data.online ? '📶 Online' : '🔴 Offline');
+    var badges = data.patron ? '⛃' : '';
 
     var mostPlayedMode = getMostPlayedMode(data.perfs, favoriteMode);
     var formattedMessage = new Discord.MessageEmbed()
         .setColor(0xFFFFFF)
-        .setAuthor({name: `${playerName}  ${status}`, iconURL: null, url: data.url})
+        .setAuthor({name: `${status}  ${playerName}  ${badges}`, iconURL: null, url: link})
         .setTitle(`Challenge ${username} to a game!`)
         .setURL(`https://lidraughts.org/?user=${data.username}#friend`)
         .addField('Games ', data.count.rated + ' rated, ' + (data.count.all - data.count.rated) + ' casual', true)
