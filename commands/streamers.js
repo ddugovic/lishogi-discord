0765e89a169fabbb4e23ca32c53f84662ea50f8a
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-16 17:22:38 -0500

    Format streamer list

--- commands/streamers.js
+++ commands/streamers.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
+const Discord = require('discord.js');
 
 async function streamers(author) {
-    url = 'https://lishogi.org/streamer/live';
-    return axios.get(url)
-        .then(response => formatStreamers(response.data))
+    return axios.get('https://lishogi.org/streamer/live')
+        .then(response => setStreamers(response.data))
         .catch((error) => {
             console.log(`Error in streamers(${author.username}): \
                 ${error.response.status} ${error.response.statusText}`);
@@ -12,12 +12,30 @@ async function streamers(author) {
         });
 }
 
+function setStreamers(data) {
+    if (data.length) {
+        const embed = new Discord.MessageEmbed()
+            .setColor(0xFFFFFF)
+            .setThumbnail('https://lishogi1.org/assets/logo/lishogi-favicon-64.png')
+            .setTitle(`:satellite: Lishogi Streamers`)
+            .setURL('https://lishogi.org/streamer')
+            .addFields(formatStreamers(data));
+        return { embeds: [ embed ] };
+    } else {
+        return 'No streamers are currently live.';
+    }
+}
+
 function formatStreamers(data) {
     var streamers = [];
-    for (var i = 0; i < data.length; i++) {
-        streamers.push(data[i].title ? `${data[i].title} ${data[i].name}` : data[i].name);
+    for (streamer of data) {
+        streamers.push({ name : formatName(streamer), value: `[Profile](https://lishogi.org/@/${streamer.name})`, inline: true })
     }
-    return streamers.length ? streamers.join('\n') : 'No streamers are currently live.';
+    return streamers;
+}
+
+function formatName(streamer) {
+    return streamer.title ? `${streamer.title} ${streamer.name}` : streamer.name;
 }
 
 function process(bot, msg, mode) {
