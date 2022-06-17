f19452c850989ab43205711c8e81b0af369ecc39
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-17 11:45:07 -0500

    Fix typo

--- commands/profile.js
+++ commands/profile.js
@@ -152,7 +152,7 @@ function formatPerfs(perfs, mode) {
         prog = `  ▼**${Math.abs(prog)}**📉`;
     else
         prog = '';
-    return `**${rating}** ± **${2*rd}${prog}** over ${games}`;
+    return `**${rating}** ± **${2*rd}**${prog} over ${games}`;
 }
 
 function formatStats(data, mode, perf) {
