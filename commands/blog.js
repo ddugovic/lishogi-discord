6641beb348629683e95dc614c9f99b96fa451f1e
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-19 17:08:52 -0500

    Credit entry author if feasible

--- commands/blog.js
+++ commands/blog.js
@@ -7,7 +7,6 @@ async function blog(author) {
     return read('https://lichess.org/blog.atom')
         .then(feed => formatBlog(feed))
         .catch(error => {
-            console.log(error)
             console.log(`Error in blog(${author.username}): \
                 ${error.response.status} ${error.response.statusText}`);
             return `An error occurred handling your request: \
@@ -17,13 +16,21 @@ async function blog(author) {
 
 function formatBlog(blog) {
     const entry = blog.entries[0];
+    const author = entry.author ?? blog.title;
+    const link = getLink(author) ?? blog.link;
     const embed = new Discord.MessageEmbed()
-        .setAuthor({name: blog.title, iconURL: 'https://lichess1.org/assets/logo/lichess-favicon-32-invert.png', url: blog.link})
+        .setAuthor({name: author, iconURL: 'https://lichess1.org/assets/logo/lichess-favicon-32-invert.png', url: link})
         .setTitle(entry.title)
         .setURL(entry.link)
         .setThumbnail('https://lichess1.org/assets/logo/lichess-favicon-64.png')
         .setDescription(entry.description);
     return { embeds: [ embed ] };
+}
+
+function getLink(author) {
+    for (match of author.matchAll(/@(\w+)/g)) {
+        return `https://lichess.org/@/${match[1]}`;
+    }
 }
 
 function process(bot, msg) {
