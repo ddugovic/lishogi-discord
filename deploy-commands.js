2b687c0d6748d42fb1e8ff55fe7aaad796304052
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-19 21:12:41 -0500

    Update leaderboard usage

--- deploy-commands.js
+++ deploy-commands.js
@@ -49,7 +49,7 @@ const commands = [
     new SlashCommandBuilder().setName('arena').setDescription("Find an upcoming or recent arena").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...modes)),
     new SlashCommandBuilder().setName('blog').setDescription("Display latest blog entry"),
     new SlashCommandBuilder().setName('deleteuser').setDescription("Delete your lidraughts username from the bot's database"),
-    new SlashCommandBuilder().setName('leaderboard').setDescription("Display the leaderboard top player").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...modes)),
+    new SlashCommandBuilder().setName('leaderboard').setDescription("Display top-rated players").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...modes)),
     new SlashCommandBuilder().setName('playing').setDescription("Share your (or a user's) current game URL").addStringOption(option => option.setName('username').setDescription('Enter lidraughts player username')),
     new SlashCommandBuilder().setName('gif').setDescription("Share your (or a user's) current game as a GIF").addStringOption(option => option.setName('username').setDescription('Enter lidraughts player username')),
     new SlashCommandBuilder().setName('privacy').setDescription("View privacy policy"),
