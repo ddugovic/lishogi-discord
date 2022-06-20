41064ade7449d78abab321d18ee77e37f3cf0b4e
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-19 22:26:14 -0500

    Display last played opening

--- commands/playing.js
+++ commands/playing.js
@@ -10,7 +10,7 @@ async function playing(author, username) {
         }
         username = user.lichessName;
     }
-    const url = `https://lichess.org/api/user/${username}/current-game?moves=false&tags=false&clocks=false&evals=false&opening=false`;
+    const url = `https://lichess.org/api/user/${username}/current-game?moves=false&tags=false&clocks=false&evals=false`;
     return axios.get(url, { headers: { Accept: 'application/json' } })
         .then(response => formatGame(response.data))
         .catch(error => {
@@ -24,13 +24,16 @@ async function playing(author, username) {
 function formatGame(game) {
     if (game.status == 'started')
         return `https://lichess.org/${game.id}`;
+    console.log(game);
     const players = [game.players.white.user, game.players.black.user].map(formatPlayer).join(' - ');
-    const embed = new Discord.MessageEmbed()
+    var embed = new Discord.MessageEmbed()
         .setAuthor({ name: players, iconURL: 'https://lichess1.org/assets/logo/lichess-favicon-32-invert.png', url: `https://lichess.org/${game.id}` })
         .setThumbnail('https://lichess1.org/assets/logo/lichess-favicon-64.png')
         .setTitle(`${title(game.perf)} game #${game.id}`)
         .setURL(`https://lichess.org/${game.id}`)
         .setImage(`https://lichess1.org/game/export/gif/${game.id}.gif`);
+    if (game.opening)
+        embed = embed.setDescription(game.opening.name);
     return { embeds: [ embed ] };
 }
 
