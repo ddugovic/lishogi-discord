50e3362104c887621b6addaa93255eefab32ef20
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-20 19:09:27 -0500

    Beautify eval output

--- commands/eval.js
+++ commands/eval.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
+const Discord = require('discord.js');
 const chess = require('chessops/chess');
 const cfen = require('chessops/fen');
 const san = require('chessops/san');
 const util = require('chessops/util');
 
 async function eval(author, fen) {
-    console.log(fen);
     if (fen && cfen.parseFen(fen).isOk) {
         const url = `https://lichess.org/api/cloud-eval?fen=${fen}&multiPv=3`;
         return axios.get(url, { headers: { Accept: 'application/json' } })
@@ -22,16 +22,29 @@ async function eval(author, fen) {
 }
 
 function formatCloudEval(fen, eval) {
+    const stats = `Nodes: ${Math.floor(eval['knodes']/1000)}M, Depth: ${eval['depth']}`;
+    const variations = [];
+    for (const pv in eval.pvs)
+        variations.push(formatVariation(fen, eval.pvs[pv]));
+
+    fen = fen.replace(/ /g,'_');
+    const embed = new Discord.MessageEmbed()
+        .setAuthor({name: 'Lichess Explorer', iconURL: 'https://lichess1.org/assets/logo/lichess-favicon-32-invert.png'})
+        .setThumbnail('https://lichess1.org/assets/logo/lichess-favicon-64.png')
+        .setTitle(':cloud: Cloud Evaluation')
+        .setURL(`https://lichess.org/analysis/standard/${fen}#explorer`)
+	.setImage(`https://lichess.org/export/gif/${fen}`);
+    const data = new Discord.MessageEmbed()
+        .addField(stats, variations.join('\n'));
+    return { embeds: [ embed, data ] };
+}
+
+function formatVariation(fen, pv) {
     const setup = cfen.parseFen(fen).unwrap();
     const pos = chess.Chess.fromSetup(setup).unwrap();
     const formatter = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2, signDisplay: 'always' });
-    var message = [`Nodes: ${Math.floor(eval['knodes']/1000)}M, Depth: ${eval['depth']}`];
-    for (const pv in eval.pvs) {
-        const variation = eval.pvs[pv]['moves'].split(' ').map(uci => util.parseUci(uci));
-        message.push(`${formatter.format(eval.pvs[pv]['cp']/100)}: ${san.makeSanVariation(pos, variation)}`);
-    }
-    message.push(`https://lichess.org/analysis/standard/${fen.replace(/ /g,'_')}#explorer`);
-    return message.join('\n');
+    const variation = pv.moves.split(' ').map(uci => util.parseUci(uci));
+    return `${formatter.format(pv.cp/100)}: ${san.makeSanVariation(pos, variation)}`;
 }
 
 function process(bot, msg, fen) {
