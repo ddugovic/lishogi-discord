b2b0778624b4147ee90fccba9593cb958ea0dbc9
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-22 03:11:38 -0500

    Format command list

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,21 @@
 const config = require('../config.json');
 
 function help(commands) {
-    var helpText = '';
-    for (var cmd in commands) {
-        var info = config.prefix + cmd;
-        var usage = commands[cmd].usage;
-        if (usage) {
-            info += ' ' + usage;
-        }
-        var description = commands[cmd].description;
-        if (description) {
-            info += '\n\t' + description;
-        }
-        helpText += '```' + info + '```';
+    var helpText = '```';
+    for (const [cmd, command] of Object.entries(commands)) {
+        helpText += `${formatCommand(cmd, command)}\n`;
     }
+    helpText += '```';
     return `Available Commands: \n${helpText}`;
+}
+
+function formatCommand(cmd, command) {
+    var info = config.prefix + cmd;
+    if (command.usage)
+        info += ' ' + command.usage;
+    if (command.description)
+        info += '\n\t' + command.description;
+    return info;
 }
 
 function process(commands, msg, username) {
