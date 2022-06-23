3735f4e4f8f59432e728c11edf59011e3dee182e
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-23 05:48:22 -0500

    Beautify team search result

--- commands/team.js
+++ commands/team.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
+const Discord = require('discord.js');
 
 async function team(author, text) {
     if (!text)
         return 'You need to specify text to search by!';
     const url = `https://lichess.org/api/team/search?text=${text}&nb=1`;
     return axios.get(url, { headers: { Accept: 'application/json' } })
-        .then(response => formatTeams(response.data))
+        .then(response => setTeams(response.data))
         .catch(error => {
             console.log(`Error in team(${author.text}, ${text}): \
                 ${error.response.status} ${error.response.statusText}`);
@@ -14,8 +15,21 @@ async function team(author, text) {
         });
 }
 
-function formatTeams(data) {
-    return data.nbResults ? `https://lichess.org/team/${data.currentPageResults[0].id}` : 'No team found.';
+function setTeams(teams) {
+    if (teams.nbResults) {
+        return { embeds: [ teams.currentPageResults.map(formatTeam)[0] ] };
+    } else {
+        return 'No team found.';
+    }
+}
+
+function formatTeam(team) {
+    return new Discord.MessageEmbed()
+        .setAuthor({name: team.leader.name, iconURL: 'https://lichess1.org/assets/logo/lichess-favicon-32-invert.png'})
+        .setThumbnail('https://lichess1.org/assets/logo/lichess-favicon-64.png')
+        .setTitle(team.name)
+        .setURL(`https://lichess.org/team/${team.id}`)
+        .setDescription(team.description);
 }
 
 function process(bot, msg, text) {
