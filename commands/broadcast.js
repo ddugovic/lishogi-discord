612995475603b8f27f9708609d24239e8b20e7b8
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-24 17:35:15 -0500

    Beautify broadcast search result

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -1,23 +1,34 @@
 const axios = require('axios');
+const Discord = require('discord.js');
 
 async function broadcast(author) {
-    url = 'https://lichess.org/api/broadcast?nb=1';
+    const url = 'https://lichess.org/api/broadcast?nb=1';
     return axios.get(url, { headers: { Accept: 'application/json' } })
         .then(response => formatBroadcast(response.data))
         .catch(error => {
             console.log(`Error in broadcast(${author.username}): \
-                ${error.response.status} ${err.response.statusText}`);
+                ${error.response.status} ${error.response.statusText}`);
             return `An error occurred handling your request: \
-                ${error.response.status} ${err.response.statusText}`;
+                ${error.response.status} ${error.response.statusText}`;
         });
 }
 
-function formatBroadcast(data) {
-    return data['tour']['url'];
+function formatBroadcast(broadcast) {
+    console.log(broadcast);
+    if (broadcast.tour) {
+        const embed = new Discord.MessageEmbed()
+            .setTitle(broadcast.tour.name)
+            .setURL(broadcast.tour.url)
+            .setThumbnail('https://lichess1.org/assets/logo/lichess-favicon-64.png')
+            .setDescription(broadcast.tour.description);
+        return { 'embeds': [ embed ] };
+    } else {
+        return 'No broadcast found!';
+    }
 }
 
 function process(bot, msg) {
-    broadcast(msg.author).then(url => msg.channel.send(url))
+    broadcast(msg.author).then(message => msg.channel.send(message));
 }
 
 async function reply(interaction) {
