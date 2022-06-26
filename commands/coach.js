c9eff6dda7fa4675b011b0bc8f138f16e034bd46
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-26 08:57:41 -0500

    Beautify coach result

--- commands/coach.js
+++ commands/coach.js
@@ -5,7 +5,7 @@ const html2md = require('html-to-md');
 
 async function coach(author) {
     return axios.get('https://lichess.org/coach/en-US/all/login')
-        .then(response => setCoach(response.data))
+        .then(response => setCoaches(response.data))
         .catch(error => {
             console.log(`Error in coach(${author.username}): \
                 ${error.response.status} ${error.response.statusText}`);
@@ -14,27 +14,27 @@ async function coach(author) {
         });
 }
 
-function setCoach(document) {
-    document = html2md(document);
-    const pattern = /## (.*)(?:\r?\n)+(.*)(?:\r?\n)+\|\|\r?\n\|(?:-+\|)+((?:\r?\n\|(?:.+\|)+)+)\r?\n.*Active.*!\[.+\]\((.+)\)/;
-    const match = document.match(pattern);
-    if (match)
-        return formatCoach(match[1], match[2], match[3].trim(), match[4]);
-    return 'No coach found!';
+function setCoaches(document) {
+    const embeds = [];
+    const pattern = /!\[.+\]\((.+)\)(?:\r?\n)+## (.+)(?:\r?\n)+(.*)(?:\r?\n)+\|\|\r?\n\|(?:-+\|)+((?:\r?\n\|(?:.+\|)+)+)\r?\n\|Active\|/g
+    for (match of html2md(document).matchAll(pattern))
+        embeds.push(formatCoach(match[1], match[2], match[3], match[4]));
+    return embeds.length ? { embeds: embeds.slice(0, 3) } : 'No coach found!';
 }
 
-function formatCoach(name, description, coach, image) {
-    const embed = new Discord.MessageEmbed()
+function formatCoach(image, name, description, details) {
+    return new Discord.MessageEmbed()
         .setAuthor({name: 'Lichess Coach', iconURL: 'https://lichess1.org/assets/logo/lichess-favicon-32-invert.png', url: 'https://lichess.org/coach/'})
         .setTitle(name)
-        .setURL(getLink(coach))
-        .setThumbnail(image ?? 'https://lichess1.org/assets/logo/lichess-favicon-64.png')
+        .setURL(getLink(details))
+        .setThumbnail(image)
         .setDescription(description);
-    return { embeds: [ embed ] };
 }
 
 function getLink(coach) {
-    return `https://lichess.org/@/${coach.match(/\/@\/(\w+)/)[1]}`;
+    const match = coach.match(/\((\/@\/\w+)\)\|/);
+    if (match)
+        return `https://lichess.org${match[1]}`;
 }
 
 function process(bot, msg) {
