1aed5b311b7094d5b51f215e98f3bda6c31bb851
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-27 05:47:08 -0500

    Use ephemeral responses

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,7 +8,7 @@ const commands = [
     new SlashCommandBuilder().setName('privacy').setDescription("View privacy policy"),
     new SlashCommandBuilder().setName('profile').setDescription("Display your (or a user's) profile").addStringOption(option => option.setName('username').setDescription('Enter woogles player username')),
     new SlashCommandBuilder().setName('puzzle').setDescription("Display today's puzzle"),
-    new SlashCommandBuilder().setName('setuser').setDescription("Set your woogles username").addStringOption(option => option.setName('username').setDescription('Enter your woogles username')),
+    new SlashCommandBuilder().setName('setuser').setDescription("Set your woogles username").addStringOption(option => option.setName('username').setDescription('Enter your woogles username').setRequired(true)),
     new SlashCommandBuilder().setName('help').setDescription("Send a list of available commands")
 ]
     .map(command => command.toJSON());
