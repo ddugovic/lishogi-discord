47dfc408edb8b9b3489e57731104a460c3422d90
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-28 04:16:32 -0500

    Graph rating history without changing layout

--- commands/profile.js
+++ commands/profile.js
@@ -148,20 +148,18 @@ function formatTeams(teams) {
 function setPuzzleHistory(embed, username) {
     const url = `https://lichess.org/api/user/${username}/rating-history`;
     return axios.get(url, { headers: { Accept: 'application/json' } })
-        .then(response => graphPuzzleHistory(embed, response.data));
+        .then(response => embed.setImage(graphHistory(response.data)));
 }
 
-async function graphPuzzleHistory(embed, history) {
+function graphHistory(history) {
     const puzzles = history.filter(perf => perf.name == 'Puzzles')[0];
-    const dates = puzzles.points.slice(-200).map(point => new Date(point[0], point[1]-1, point[2], 0, 0, 0, 0).getTime());
-    const ratings = puzzles.points.slice(-200).map(point => point[3]);
-
-    const image = await new QuickChart().setWidth(299).setConfig({
+    const dates = puzzles.points.slice(-60).map(point => new Date(point[0], point[1]-1, point[2], 0, 0, 0, 0).getTime());
+    const ratings = puzzles.points.slice(-60).map(point => point[3]);
+    return new QuickChart().setWidth(299).setHeight(200).setConfig({
         type: 'line',
         data: { labels: dates, datasets: [{ label: 'Puzzles', data: ratings }] },
         options: { scales: { xAxes: [{ type: 'time' }] } }
-    }).getShortUrl();
-    return embed.setImage(image);
+    }).getUrl();
 }
 
 function getMostPlayedMode(perfs, favoriteMode) {
