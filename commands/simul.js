3a081d9f3c21ee5dd29dedfe98e22acfb9db0338
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-06-28 19:13:13 -0500

    Display undescribed simul

--- commands/simul.js
+++ commands/simul.js
@@ -41,11 +41,13 @@ function formatSimul(simul) {
         .setDescription(`${players} ${compete} in the ${simul.fullName}.`);
     if (simul.host.gameId)
         embed = embed.setImage(`https://lishogi1.org/game/export/gif/${simul.host.gameId}.gif`);
-    const description = formatDescription(simul.text);
-    if (description) {
-        const about = new Discord.MessageEmbed()
-            .addField('Description', description);
-        return { embeds: [ embed, about ] };
+    if (simul.text) {
+        const description = formatDescription(simul.text);
+        if (description) {
+            const about = new Discord.MessageEmbed()
+                .addField('Description', description);
+            return { embeds: [ embed, about ] };
+        }
     }
     return { embeds: [ embed ] };
 }
@@ -63,7 +65,7 @@ function formatDescription(text) {
     const links = formatLinks(text);
     const result = links.length ? [links.join(' | ')] : [];
     const about = formatAbout(text.split(/(?:\r?\n)+/));
-    if (about.length)
+    if (about.length && about.join('').length)
         result.push(about.join('\n'));
     return result.join('\n');
 }
