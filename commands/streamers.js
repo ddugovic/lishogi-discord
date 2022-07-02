587ee435298ae3a49638e9dcd6a15db69014feb4
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-02 06:33:28 -0500

    Improve streamer list performance

--- commands/streamers.js
+++ commands/streamers.js
@@ -1,10 +1,8 @@
 const axios = require('axios');
-const countryFlags = require('emoji-flags');
 const Discord = require('discord.js');
 const formatColor = require('../lib/format-color');
 const { formatLink, formatSocialLinks } = require('../lib/format-links');
 const { formatTitledUserLink, formatUserLink, formatUserLinks } = require('../lib/format-user-links');
-const formatSeconds = require('../lib/format-seconds');
 
 async function streamers(author) {
     return axios.get('https://lichess.org/streamer/live')
@@ -19,20 +17,15 @@ async function streamers(author) {
 
 function setStreamers(streamers) {
     if (streamers.length) {
-        const url = 'https://lichess.org/api/users';
-        const ids = streamers.map(streamer => streamer.id);
-        return axios.post(url, ids.join(','), { headers: { Accept: 'application/json' } })
-            .then(response => {
-                const fields = response.data.map(formatStreamer);
-                const rating = Math.max(...fields.map(field => field.rating));
-                const embed = new Discord.MessageEmbed()
-                    .setColor(getColor(rating))
-                    .setThumbnail('https://lichess1.org/assets/logo/lichess-favicon-64.png')
-                    .setTitle(`:satellite: Lichess Streamers`)
-                    .setURL('https://lichess.org/streamer')
-                    .addFields(fields.sort((a,b) => b.score - a.score));
-                return { embeds: [ embed ] };
-        });
+        const fields = streamers.map(formatStreamer);
+        const rating = Math.max(...fields.map(field => field.rating));
+        const embed = new Discord.MessageEmbed()
+            .setColor(getColor(rating))
+            .setThumbnail('https://lichess1.org/assets/logo/lichess-favicon-64.png')
+            .setTitle(`:satellite: Lichess Streamers`)
+            .setURL('https://lichess.org/streamer')
+            .addFields(fields.sort((a,b) => b.score - a.score));
+        return { embeds: [ embed ] };
     } else {
         return 'No streamers are currently live.';
     }
@@ -44,63 +37,42 @@ function getColor(rating) {
 }
 
 function formatStreamer(streamer) {
-    const [country, fideRating] = getCountryAndRating(streamer.profile) ?? [];
-    const name = formatName(streamer, country, fideRating);
+    const name = streamer.title ? `**${streamer.title}** ${streamer.name}` : streamer.name;
     const badges = streamer.patron ? '🦄' : '';
-    const [profile, rating, score] = formatProfile(streamer.username, streamer.profile, fideRating, streamer.playTime);
+    const [profile, rating, score] = formatStream(streamer.username, streamer.title, streamer.streamer, streamer.stream);
     return { name : `${name} ${badges}`, value: profile, inline: true, 'rating': rating, 'score': score };
 }
 
-function getCountryAndRating(profile) {
-    if (profile)
-        return [profile.country, profile.fideRating];
-}
+function formatStream(username, title, streamer, stream) {
+    const links = [`[Profile](https://lichess.org/@/${username})`];
+    if (streamer.twitch)
+        links.push(formatSocialLinks(streamer.twitch));
+    if (streamer.youTube)
+        links.push(formatSocialLinks(streamer.youTube));
 
-function formatName(streamer, country, rating) {
-    var name = getLastName(streamer.profile) ?? streamer.username;
-    if (streamer.title)
-        name = `**${streamer.title}** ${name}`;
-    if (country && countryFlags.countryCode(country))
-        name = `${countryFlags.countryCode(country).emoji} ${name}`;
-    if (rating)
-        name += ` (${rating})`;
-    return name;
-}
-
-function getLastName(profile) {
-    if (profile)
-        return profile.lastName;
-}
-
-function formatProfile(username, profile, fideRating, playTime) {
-    const duration = formatSeconds(playTime ? playTime.tv : 0).split(', ')[0];
-    const links = profile ? formatSocialLinks(profile.links ?? profile.bio ?? '') : [];
-    links.unshift(`[Profile](https://lichess.org/@/${username})`);
-
-    const result = [`Time on :tv:: ${duration.replace('minutes','min.').replace('seconds','sec.')}`];
-    result.push(links.join(' | '));
+    const result = [stream.status, links.join(' | ')];
     var length = 0;
     var rating = 0;
-    if (profile && profile.bio) {
-        const bio = formatBio(profile.bio.split(/\s+/));
-        if ((length = bio.length)) {
-            rating = fideRating ?? 1000;
-            result.push(bio);
+    if (streamer.headline && streamer.description) {
+        const text = `*${streamer.headline}*\n${formatDescription(streamer.description.split(/\s+/))}`;
+        if ((length = text.length)) {
+            rating = title == 'GM' ? 2500 : title == 'IM' ? 2400 : title == 'FM' ? 2300 : title ? 2200 : 1000;
+            result.push(text);
 	}
     }
-    return [result.join('\n'), rating, ((length + rating) * 1000000 + playTime.tv * 1000 + playTime.total)];
+    return [result.join('\n'), rating, (length + rating) * 1000000];
 }
 
-function formatBio(bio) {
+function formatDescription(text) {
     const social = /:\/\/|\btwitch\.tv\b|\byoutube\.com\b|\byoutu\.be\b/i;
-    for (let i = 0; i < bio.length; i++) {
-        if (bio[i].match(social)) {
-            bio = bio.slice(0, i);
+    for (let i = 0; i < text.length; i++) {
+        if (text[i].match(social)) {
+            text = text.slice(0, i);
             break;
         }
-        bio[i] = formatUserLinks(bio[i]);
+        text[i] = formatUserLinks(text[i]);
     }
-    return bio.join(' ');
+    return text.join(' ');
 }
 
 function process(bot, msg, mode) {
