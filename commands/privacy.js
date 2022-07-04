9c7bb1f96c93cde9364b8f1dfe55681e332710d8
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-04 07:22:04 -0500

    Mention owner in privacy command

--- commands/privacy.js
+++ commands/privacy.js
@@ -1,11 +1,11 @@
 const message = 'This bot only stores your Chess.com username and favorite game mode. Contact <@161060630726574080> if you need your data removed and cannot figure out how to use the deleteuser command.';
 
 function process(bot, msg, username) {
-    msg.channel.send(message, { allowedMentions: { users : [] } });
+    msg.channel.send(message);
 }
 
 function reply(interaction) {
-    return { content: message, allowedMentions: { users: [] } };
+    return message;
 }
 
 module.exports = { process, reply };
