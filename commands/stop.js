777277b2af5fccaa13571122d2993db77ec41289
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-04 22:04:35 -0500

    Secure stop command

--- commands/stop.js
+++ commands/stop.js
@@ -1,16 +1,8 @@
 const config = require('../config.json');
 
-function process(bot, msg) {
-    if (msg.author.id == config.owner) {
+function stop(bot, msg) {
+    if (msg.author.username == config.ownerId)
         process.exit();
-    }
 }
 
-function reply(interaction) {
-    if (interaction.author.id == config.owner) {
-        process.exit();
-    }
-    return '';
-}
-
-module.exports = {process, reply};
+module.exports = stop;
