e0cf8271dd0510f0041dcdfbe7b4259fc0332596
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-06 05:34:11 -0500

    Display patron simul host

--- commands/simul.js
+++ commands/simul.js
@@ -61,7 +61,8 @@ function getColor(rating) {
 }
 
 function formatHost(player) {
-    return player.title ? `${player.title} ${player.name}` : player.name;
+    const badges = player.patron ? '⛩️' : '';
+    return player.title ? `${player.title} ${player.name} ${badges}` : `${player.name} ${badges}`;
 }
 
 function getImage(host) {
