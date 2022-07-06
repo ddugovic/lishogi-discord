c5f7f0103c7f609d57c8573040079ef3fbfcaf74
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-06 06:07:08 -0500

    Simplify team pagination

--- commands/team.js
+++ commands/team.js
@@ -1,22 +1,20 @@
 const axios = require('axios');
-const Discord = require('discord.js');
-const headlineParser = require('eklem-headline-parser')
+const { MessageEmbed } = require('discord.js');
 const formatColor = require('../lib/format-color');
-const { formatLink, formatSocialLinks } = require('../lib/format-links');
+const { formatSocialLinks } = require('../lib/format-links');
 const formatPages = require('../lib/format-pages');
 const { formatSiteLinks } = require('../lib/format-site-links');
+const fn = require('friendly-numbers');
 const plural = require('plural');
-const removeAccents = require('remove-accents');
-const removeMarkdown = require("remove-markdown");
-const lda = require('@stdlib/nlp-lda');
-const stopwords = require('@stdlib/datasets-stopwords-en');
 
 function team(author, text, interaction) {
+    if (!text)
+        return 'You need to specify text to search by!';
     text = text.replace(/\s+/, '');
     const url = `https://lishogi.org/api/team/search?text=${text}`;
     return axios.get(url, { headers: { Accept: 'application/json' } })
-        .then(response => setTeams(response.data, text, interaction))
-        .then(embeds => formatPages(embeds, interaction, 'No teams found.'))
+        .then(response => response.data.currentPageResults.map(formatTeam))
+        .then(embeds => formatPages(embeds, interaction, 'No team found.'))
         .catch(error => {
             console.log(`Error in team(${author.text}, ${text}): \
                 ${error.response.status} ${error.response.statusText}`);
@@ -25,59 +23,24 @@ function team(author, text, interaction) {
         });
 }
 
-function setTeams(teams, text, interaction) {
-    text = removeAccents(text).toLowerCase();
-    teams.currentPageResults.forEach(team => team.score = score(team, text));
-    return teams.currentPageResults.sort((a,b) => b.score - a.score).map(formatTeam);
-}
-
-function score(team, text) {
-    const description = cleanDescription(formatDescription(removeAccents(team.description).toLowerCase()));
-    const links = removeMarkdown(removeImages(description)).matchAll(/(https?:\/\/[^\s]+)/g);
-    const noise = [...links].reduce((partialSum, a) => partialSum + a[0].length, 0);
-
-    const prose = strip(removeMarkdown(description).replace(/(https?:\/\/[^\s]+)/g, ''));
-    const docs = prose.replaceAll(/[^\s\w]+/g, ' ').trim().split(/(?:\r?\n)+/);
-    const topics = getTopics(docs, prose);
-    return team.nbMembers * (docs.length * 10 - noise) * topics.map(topic => scoreTopic(topic, prose)).reduce((partialSum, a) => partialSum + a, 0);
-}
-
-function removeImages(text) {
-    return text.replaceAll(/!\[\]\(\S+\)/g, '');
-}
-
-function strip(description) {
-    for (word of stopwords())
-        description = description.replaceAll(` ${word} `, ' ');
-    return description;
-}
-
-function getTopics(docs, text) {
-    const model = lda(docs, 10);
-    model.fit(1000, 100, 10)
-    const topics = [];
-    for (i = 0; i < 10; i++) { topics.push(model.getTerms(0, 20)); }
-    return topics;
-}
-
-function scoreTopic(topic, text) {
-    return topic.filter(term => term.word == text).map(term => term.prob)[0] ?? 0;
-}
-
 function formatTeam(team) {
     const count = Math.min(Math.max(Math.floor(team.nbMembers / 100), 0), 255);
     const description = formatDescription(team.description);
-    return new Discord.MessageEmbed()
+    return new MessageEmbed()
         .setColor(formatColor(count, 0, 255-count))
         .setThumbnail(getImage(team.description) ?? 'https://lishogi1.org/assets/logo/lishogi-favicon-64.png')
         .setTitle(team.name)
         .setURL(`https://lishogi.org/team/${team.id}`)
         .setDescription(cleanDescription(description))
-        .addField(plural('Leader', team.leaders.length), team.leaders.map(formatLeader).join(', '));
+        .addField('Members', `**${fn.format(team.nbMembers)}**`, true)
+        .addField(plural('Leader', team.leaders.length), team.leaders.map(formatLeader).join(', '), true);
 }
 
 function cleanDescription(description) {
-    return description.split(/\r?\n/).map(formatSiteLinks).map(formatLink).join('\n');
+    const lines = description.split(/\r?\n/).map(formatSiteLinks);
+    while (lines.join('\n').length > 4000)
+        lines.pop();
+    return lines.join('\n');
 }
 
 function formatDescription(text) {
@@ -113,8 +76,6 @@ function formatLeader(user) {
 }
 
 function process(bot, msg, text) {
-    if (!text)
-        return msg.channel.send('You need to specify text to search by!');
     team(msg.author, text).then(message => msg.channel.send(message));
 }
 
