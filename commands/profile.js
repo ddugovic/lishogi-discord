fd2adde6c76484455908c02ed498fe2bb06b5ca6
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-09 23:12:30 -0500

    Code cleanup

--- commands/profile.js
+++ commands/profile.js
@@ -137,7 +137,7 @@ function setTeams(embed, username) {
     return axios.get(url, { headers: { Accept: 'application/json' } })
         .then(response => {
             const teams = formatTeams(response.data);
-            return teams ? embed.addField('Teams', teams, true) : embed;
+            return teams ? embed.addField('Teams', teams) : embed;
         });
 }
 
