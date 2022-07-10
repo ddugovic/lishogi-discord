806064dcf47cd862000306d2c555d4ac5915c05b
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-10 06:33:57 -0500

    Format recent game handicap

--- commands/profile.js
+++ commands/profile.js
@@ -300,7 +300,27 @@ function formatGame(game) {
     const url = `https://lishogi.org/${game.id}`;
     const score = game.winner == 'sente' ? ['○', '●'] : game.winner = 'gote' ? ['●', '○'] : [' ', ' '];
     const players = [game.players.sente, game.players.gote].map(formatPlayerName).join(' - ');
-    return `${formatClock(game.clock)} ${score[0]}[${players}](${url})${score[1]}`;
+    return `${formatClock(game.clock)} ${score[0]}[${players}](${url})${score[1]} (${formatHandicap(game)})`;
+}
+
+function formatHandicap(game) {
+    const handicaps = {
+        'lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '平手',
+        'lnsgkgsn1/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '香落ち',
+        '1nsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '右香落ち',
+        'lnsgkgsnl/1r7/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '角落ち',
+        'lnsgkgsnl/7b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '飛車落ち',
+        'lnsgkgsn1/7b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '飛香落ち',
+        'lnsgkgsnl/9/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '二枚落ち',
+        'lnsgkgsn1/9/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '三枚落ち',
+        '1nsgkgsn1/9/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '四枚落ち',
+        '2sgkgsn1/9/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '五枚落ち',
+        '1nsgkgs2/9/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '左五枚落ち',
+        '2sgkgs2/9/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '六枚落ち',
+        '3gkg3/9/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '八枚落ち',
+        '4k4/9/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1': '十枚落ち',
+    };
+    return handicaps[game.initialFen ?? 'lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1'] ?? 'その他';
 }
 
 function formatPlayerName(player) {
