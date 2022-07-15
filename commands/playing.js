5bd60b357f303350d4623bc658122bf54fa7469e
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-15 07:47:04 -0500

    Describe current no-moves game

--- commands/playing.js
+++ commands/playing.js
@@ -12,10 +12,10 @@ async function playing(author, username) {
     }
     const url = `https://lichess.org/api/user/${username}/current-game?evals=false`;
     return axios.get(url, { headers: { Accept: 'application/json' } })
-        .then(response => formatGame(response.data))
+        .then(response => formatCurrentGame(response.data))
         .then(embed => { return { embeds: [ embed ] } })
         .catch(error => {
-            console.log(`Error in playing(${author.username}): \
+            console.log(`Error in playing(${author.username}, ${username}): \
                 ${error.response.status} ${error.response.statusText}`);
             return `An error occurred handling your request: \
                 ${error.response.status} ${error.response.statusText}`;
@@ -28,18 +28,17 @@ async function getName(author) {
         return user.lichessName;
 }
 
-function formatGame(game) {
+function formatCurrentGame(game) {
     const players = [game.players.white.user, game.players.black.user].map(formatPlayer).join(' - ');
     var embed = new Discord.MessageEmbed()
         .setColor(getColor(game.players))
         .setAuthor({ name: players, iconURL: 'https://lichess1.org/assets/logo/lichess-favicon-32-invert.png', url: `https://lichess.org/${game.id}` })
         .setThumbnail('https://lichess1.org/assets/logo/lichess-favicon-64.png')
         .setTitle(`${title(game.perf)} game #${game.id}`)
-        .setURL(`https://lichess.org/${game.id}`);
+        .setURL(`https://lichess.org/${game.id}`)
+        .setDescription(formatGame(game));
     if (game.status != 'started')
         embed = embed.setImage(`https://lichess1.org/game/export/gif/${game.id}.gif`);
-    if (game.moves)
-        embed = embed.setDescription(formatOpening(game.opening, game.moves));
     return embed;
 }
 
@@ -55,10 +54,22 @@ function formatPlayer(player) {
     return player.name;
 }
 
-function formatOpening(opening, moves) {
-    const ply = opening ? opening.ply : 10;
-    const variation = moves.split(/ /).slice(0, ply);
-    return opening ? `${opening.name}\n*${formatSanVariation(null, variation)}*` : `*${numberVariation(variation)}*`;
+function formatGame(game) {
+    const opening = game.moves ? ` ${formatOpening(game.opening, game.initialFen, game.moves)}` : '';
+    return `${formatClock(game.clock, game.daysPerTurn)} <t:${Math.floor(game.createdAt / 1000)}:R>${opening}`;
+}
+
+function formatOpening(opening, initialFen, moves) {
+    const variation = moves.split(/ /).slice(0, opening ? opening.ply : 10);
+    return opening ? `${opening.name}\n*${formatSanVariation(initialFen, variation)}*` : `*${numberVariation(variation)}*`;
+}
+
+function formatClock(clock, daysPerTurn) {
+    if (clock) {
+        const base = clock.initial == 15 ? '¼' : clock.initial == 30 ? '½' : clock.initial == 45 ? '¾' : clock.initial / 60;
+        return `${base}+${clock.increment}`;
+    }
+    return daysPerTurn ? `${daysPerTurn} ${plural('day', daysPerTurn)}` : '∞';
 }
 
 function title(str) {
