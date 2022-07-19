1d4ba4c62df721dd02bb7f718696894d3cfebda3
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-18 20:33:56 -0500

    Code cleanup

--- commands/team.js
+++ commands/team.js
@@ -1,22 +1,19 @@
 const axios = require('axios');
-const Discord = require('discord.js');
-const headlineParser = require('eklem-headline-parser')
+const { MessageEmbed } = require('discord.js');
 const formatColor = require('../lib/format-color');
 const { formatSocialLinks } = require('../lib/format-links');
 const { formatUserLink, formatUserLinks } = require('../lib/format-site-links');
+const formatPages = require('../lib/format-pages');
 const plural = require('plural');
-const removeAccents = require('remove-accents');
-const removeMarkdown = require("remove-markdown");
-const lda = require('@stdlib/nlp-lda');
-const stopwords = require('@stdlib/datasets-stopwords-en');
 
-async function team(author, text) {
+function team(author, text, interaction) {
     if (!text)
         return 'You need to specify text to search by!';
     text = text.replace(/\s+/, '');
     const url = `https://lidraughts.org/api/team/search?text=${text}`;
     return axios.get(url, { headers: { Accept: 'application/json' } })
-        .then(response => setTeams(response.data, text))
+        .then(response => response.data.currentPageResults.map(formatTeam))
+        .then(embeds => formatPages(embeds, interaction, 'No team found.'))
         .catch(error => {
             console.log(`Error in team(${author.text}, ${text}): \
                 ${error.response.status} ${error.response.statusText}`);
@@ -25,46 +22,10 @@ async function team(author, text) {
         });
 }
 
-function setTeams(teams, text) {
-    text = removeAccents(text).toLowerCase();
-    if (teams.nbResults) {
-        teams.currentPageResults.forEach(team => team.score = score(team, text));
-        const team = teams.currentPageResults.sort((a,b) => b.score - a.score)[0];
-        return { embeds: [ formatTeam(team) ] };
-    } else {
-        return 'No team found.';
-    }
-}
-
-function score(team, text) {
-    const description = strip(removeAccents(removeMarkdown(team.description)).toLowerCase());
-    const docs = description.replaceAll(/[^\s\w]+/g, ' ').trim().split(/(?:\r?\n)+/);
-    const topics = getTopics(docs, text);
-    return team.nbMembers * docs.length * topics.map(topic => scoreTopic(topic, text)).reduce((partialSum, a) => partialSum + a, 0);
-}
-
-function strip(description) {
-    for (word of stopwords())
-        description = description.replaceAll(` ${word} `, ' ');
-    return description;
-}
-
-function getTopics(docs, text) {
-    const model = lda(docs, 10);
-    model.fit(1000, 100, 10)
-    const topics = [];
-    for (i = 0; i < 10; i++) { topics.push(model.getTerms(0, 20)); }
-    return topics;
-}
-
-function scoreTopic(topic, text) {
-    return topic.filter(term => term.word == text).map(term => term.prob)[0] ?? 0;
-}
-
 function formatTeam(team) {
     const count = Math.min(Math.max(Math.floor(team.nbMembers / 100), 0), 255);
     const description = formatDescription(team.description);
-    return new Discord.MessageEmbed()
+    return new MessageEmbed()
         .setColor(formatColor(count, 0, 255-count))
         .setThumbnail(getImage(team.description) ?? 'https://lidraughts.org/assets/favicon.64.png')
         .setTitle(team.name)
@@ -120,8 +81,8 @@ function process(bot, msg, text) {
     team(msg.author, text).then(message => msg.channel.send(message));
 }
 
-async function reply(interaction) {
-    return team(interaction.user, interaction.options.getString('text'));
+function interact(interaction) {
+    team(interaction.user, interaction.options.getString('text'));
 }
 
-module.exports = {process, reply};
+module.exports = {process, interact};
