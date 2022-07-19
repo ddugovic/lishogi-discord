bc2c82e393ce9ce6445bd91dc1330b894c44da91
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-18 21:17:24 -0500

    Define stop application guild command

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,5 @@
 const config = require('./config.json');
+const { PermissionFlagsBits } = require('discord-api-types/v10');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const channels = [
     { name: 'Top Rated', value: 'Top Rated' },
@@ -28,6 +29,12 @@ const ratings = [
     { name: 'Rapid', value: 'rapid' },
     { name: 'UltraBullet', value: 'ultraBullet' }
 ];
+
+const guildCommands = [
+    new SlashCommandBuilder().setName('stop').setDescription("Stop the bot (owner only)")
+]
+    .map(command => command.setDefaultMemberPermissions(PermissionFlagsBits.Administrator).toJSON());
+
 const commands = [
     new SlashCommandBuilder().setName('arena').setDescription("Find an upcoming or recent arena").addStringOption(option => option.setName('mode').setDescription('Enter a game mode').addChoices(...modes)),
     new SlashCommandBuilder().setName('blog').setDescription("Display recent blog entries"),
@@ -51,11 +58,15 @@ const commands = [
     new SlashCommandBuilder().setName('video').setDescription("Search videos for a keyword").addStringOption(option => option.setName('text').setDescription('Search keywords')),
     new SlashCommandBuilder().setName('help').setDescription("Display a list of available commands")
 ]
-    .map(command => command.toJSON());
+    .map(command => command.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages).toJSON());
 
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const rest = new REST({ version: '10' }).setToken(config.token);
+
+rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: guildCommands })
+    .then(() => console.log(`Successfully registered ${guildCommands.length} application guild slash commands for client ${config.clientId}.`))
+    .catch(console.error);
 
 rest.put(Routes.applicationCommands(config.clientId), { body: commands })
     .then(() => console.log(`Successfully registered ${commands.length} application slash commands for client ${config.clientId}.`))
