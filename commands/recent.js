932870d8f5fd5b31394cf39756184a52dbfba1c7
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-23 10:41:23 -0500

    Paginate recent games

--- commands/recent.js
+++ commands/recent.js
@@ -9,7 +9,7 @@ const User = require('../models/User');
 
 async function recent(username, interaction) {
     const url = `https://woogles.io/twirp/game_service.GameMetadataService/GetRecentGames`;
-    const request = { username: username, numGames: 1, offset: 0 };
+    const request = { username: username, numGames: 10, offset: 0 };
     const headers = { authority: 'woogles.io', accept: 'application/json', origin: 'https://woogles.io' };
     return axios.post(url, request, { headers: headers })
         .then(response => formatPages(response.data.game_info.map(formatGame), interaction, 'No games found!'))
