1f9d496b430954ee6058c546528726960c5533fb
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-07-30 05:14:07 -0500

    Code cleanup

--- commands/news.js
+++ commands/news.js
@@ -41,9 +41,9 @@ function formatEntry(entry, link) {
     const url = getURL(entry);
     if (url)
         embed = embed.setURL(url);
-    const image = getImageURL(entry);
+    const image = getImage(html2md(content));
     if (image)
-        embed = embed.setImage(image);
+        embed = embed.setThumbnail(image);
     return embed;
 }
 
