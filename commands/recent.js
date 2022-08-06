b027f0e64c8ba7ddbb5a48dffa418757c2fe3ca3
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-08-06 10:25:12 -0500

    Recent games show challenge events

--- commands/recent.js
+++ commands/recent.js
@@ -35,7 +35,7 @@ function formatHistory(playerNicknames, history) {
     const plays = [];
     var last;
     for (const event of history.events) {
-        if (event.type == 'CHALLENGE' || event.type == 'CHALLENGE_BONUS' || event.type == 'TILE_PLACEMENT_MOVE' || event.type == 'TIME_PENALTY' || event.type == 'UNSUCCESSFUL_CHALLENGE_TURN_LOSS')
+        if (['CHALLENGE', 'CHALLENGE_BONUS', 'TILE_PLACEMENT_MOVE', 'TIME_PENALTY', 'UNSUCCESSFUL_CHALLENGE_TURN_LOSS'].contains(event.type))
             plays.push((last = event));
         else if (event.type == 'PHONY_TILES_RETURNED')
             last.invalid = true;
@@ -57,8 +57,8 @@ function formatEvent(event) {
         const bingo = formatWord(event.words_formed[0], event.played_tiles);
         return `${event.position} ${bingo}${event.invalid ? '*' : ''} **${event.score}**`;
     }
-    if (event.type == 'CHALLENGE' || event.type == 'CHALLENGE_BONUS' || 'UNSUCCESSFUL_CHALLENGE_TURN_LOSS')
-        return `Challenge **${event.score - event.lost_score}**`;
+    if (['CHALLENGE', 'CHALLENGE_BONUS', 'UNSUCCESSFUL_CHALLENGE_TURN_LOSS'].contains(event.type))
+        return `Challenge **${event.score + event.lost_score}**`;
     if (event.type == 'TIME_PENALTY')
         return `Time Penalty **${event.lost_score}**`;
 }
