9be13d2ec0b4193642314e11f34089f51eea410b
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-08-13 18:13:34 -0500

    Fix news URL

--- commands/news.js
+++ commands/news.js
@@ -8,7 +8,7 @@ function news(author, interaction) {
     const url = 'http://www.thechessmind.net/blog/rss.xml';
     return axios.get(url, { headers: { Accept: 'application/rss+xml' } })
         .then(response => parseFeed(response.data))
-        .then(feed => formatEntries(feed))
+        .then(feed => formatEntries(feed.channel))
         .then(embeds => formatPages(embeds, interaction, 'No news found!'))
         .catch(error => {
             console.log(`Error in news(${author.username}): \
@@ -18,15 +18,15 @@ function news(author, interaction) {
         });
 }
 
-function formatEntries(feed) {
+function formatEntries(channel) {
     const embeds = [];
-    for (const entry of feed.channel.item.values()) {
+    for (const entry of channel.item.values()) {
         const timestamp = Math.floor(new Date(entry.pubDate).getTime() / 1000);
         const summary = formatContent(entry.description, 120).replace(/published here/, `published [here](https://thechessmind.substack.com/)`);
         const red = Math.min(Math.max(summary.length - 150, 0), 255);
         var embed = new EmbedBuilder()
             .setColor(formatColor(red, 0, 255-red))
-            .setAuthor({name: entry['dc:creator'], iconURL: 'https://lichess1.org/assets/logo/lichess-favicon-32-invert.png', link: feed.link})
+            .setAuthor({name: entry['dc:creator'], iconURL: 'https://lichess1.org/assets/logo/lichess-favicon-32-invert.png', url: channel.link})
             .setTitle(entry.title)
             .setURL(entry.link)
             .setDescription(`<t:${timestamp}:F>\n${summary}`);
