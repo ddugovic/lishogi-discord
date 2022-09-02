98c9363fdd29a9c4af32683c720542d5ae81593e
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-09-01 19:19:51 -0500

    Beautify TV search result

--- commands/tv.js
+++ commands/tv.js
@@ -1,28 +1,70 @@
 const axios = require('axios');
+const { EmbedBuilder } = require('discord.js');
+const formatClock = require('../lib/format-clock');
+const formatColor = require('../lib/format-color');
 const User = require('../models/User');
 
 async function tv(author, mode) {
-    if (!mode) {
-        const user = await User.findById(author.id).exec();
-        mode = (user && user.favoriteMode) ? user.favoriteMode : 'blitz';
-    }
+    if (!mode)
+        mode = await getMode(author);
     const url = 'https://lishogi.org/tv/channels';
-    return axios.get(url, { headers: { Accept: 'application/vnd.lishogi.v3+json' } })
-        .then(response => formatTv(response.data, mode))
-        .catch((err) => {
+    return axios.get(url, { headers: { Accept: 'application/json' } })
+        .then(response => {
+            if ((channel = getChannel(response.data, mode || 'Top Rated'))) {
+                return formatChannel(...channel);
+            }
+        })
+        .then(embed => { return embed ? { embeds: [ embed ] } : 'Channel not found!' })
+        .catch(error => {
             console.log(`Error in tv(${author.username}, ${mode}): \
-                ${err.response.status} ${err.response.statusText}`);
+                ${error.response.status} ${error.response.statusText}`);
             return `An error occurred handling your request: \
-                ${err.response.status} ${err.response.statusText}`;
+                ${error.response.status} ${error.response.statusText}`;
         });
 }
 
-function formatTv(data, mode) {
-    for (channel in data) {
-        if (channel.toLowerCase() == mode.toLowerCase())
-            return `https://lishogi.org/${data[channel].gameId}`;
+async function getMode(author) {
+    const user = await User.findById(author.id).exec();
+    if (user)
+        return user.favoriteMode;
+}
+
+function getChannel(data, mode) {
+    for (const [channel, tv] of Object.entries(data)) {
+        if (camel(channel).toLowerCase() == camel(mode).toLowerCase())
+            return [channel == 'Top Rated' ? 'best' : camel(channel), channel, tv];
     }
-    return `No channel of mode ${mode} found!`;
+}
+
+function formatChannel(channel, name, tv) {
+    const user = formatUser(tv.user);
+    return new EmbedBuilder()
+        .setColor(getColor(tv.rating))
+        .setAuthor({name: user.replace(/\*\*/g, ''), iconURL: 'https://lishogi1.org/assets/logo/lishogi-favicon-32-invert.png', url: `https://lishogi.org/@/${tv.user.name}`})
+        .setThumbnail(`https://lishogi1.org/game/export/gif/thumbnail/${tv.gameId}.gif`)
+        .setTitle(`${name} :tv: ${user} (${tv.rating})`)
+        .setURL(`https://lishogi.org/tv/${channel}`)
+        .setDescription(`Sit back, relax, and watch the best ${name} games on Lishogi!`);
+}
+
+function getColor(rating) {
+    const red = Math.min(Math.max(Math.floor((rating - 2000) / 2), 0), 255);
+    return formatColor(red, 0, 255-red);
+}
+
+function formatPlayer(player) {
+    return player.user ? formatUser(player.user) : player.aiLevel ? `AI level ${player.aiLevel}` : 'Anonymous';
+}
+
+function formatUser(user) {
+    return user.title ? `**${user.title}** ${user.name}` : user.name;
+}
+
+function camel(str) {
+    str = str.split(/\W/)
+        .map((x) => (x.charAt(0).toUpperCase() + x.slice(1)))
+        .join('');
+    return str.charAt(0).toLowerCase() + str.slice(1);
 }
 
 function process(bot, msg, mode) {
