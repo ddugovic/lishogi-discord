51623b38492c7e05723eda00770c18dc6a735534
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-09-11 18:21:06 -0500

    Format simple reddit tables

--- test/test-table.js
+++ test/test-table.js
@@ -12,7 +12,7 @@ describe('Commands', function() {
 4     GM Liren Ding          2806    3
 4     GM Richard Rapport     2764    3
 7     GM Radjabov Teimour    2753    2,5
-8     GM Alireza Firouzja    2793    2,5    \`\`\``, formatTable('|Rank|Player|Rating|Score|',
+8     GM Alireza Firouzja    2793    2,5\`\`\``, formatTable('|Rank|Player|Rating|Score|',
 `|1|GM Ian Nepomniachtchi|2766|5,5|
 |2|GM Fabiano Caruana|2783|5|
 |3|GM Hikaru Nakamura|2760|3,5|
