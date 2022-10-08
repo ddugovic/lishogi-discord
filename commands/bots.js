462f011639b63a1e2c6f11458dc559f4317e839e
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-10-08 10:52:13 -0500

    Fix (already-fixed) axios BC breakage

--- commands/bots.js
+++ commands/bots.js
@@ -11,7 +11,8 @@ const User = require('../models/User');
 
 async function bots(author, interaction) {
     const mode = await getMode(author) || 'blitz';
-    return axios.get('https://lichess.org/api/bot/online?nb=50', { headers: { Accept: 'application/x-ndjson' } })
+    const url = 'https://lichess.org/api/bot/online';
+    return axios.get(url, { headers: { Accept: 'application/x-ndjson' }, params: { nb: 50 } })
         .then(response => filter(parseDocument(response.data)))
         .then(bots => bots.map(bot => formatBot(bot, mode)))
         .then(embeds => formatPages(embeds, interaction, 'No bots are currently online.'))
