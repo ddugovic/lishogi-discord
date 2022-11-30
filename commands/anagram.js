07fe369481d581321aa451267e58282d6e70f119
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-11-30 06:18:39 -0600

    Fetch anagrams with definitions

--- commands/anagram.js
+++ commands/anagram.js
@@ -1,19 +1,18 @@
-const axios = require('axios');
 const { EmbedBuilder } = require('discord.js');
 const { formatLexicon } = require('../lib/format-lexicon');
 const formatPages = require('../lib/format-pages');
 
 async function anagram(lexicon, alphagrams, interaction) {
     const url = 'https://woogles.io/twirp/word_service.WordService/DefineWords';
-    const headers = { authority: 'woogles.io', origin: 'https://woogles.io' };
-    const request = { lexicon: lexicon, words: alphagrams.split(/[\s,]+/), definitions: true, anagrams: true };
-    return axios.post(url, request, { headers: headers })
-        .then(response => formatPages('Alphagram', Object.entries(response.data.results).map(entry => formatEntry(lexicon, ...entry)), interaction, 'Alphagrams not found!'))
+    const headers = { accept: 'application/json', authority: 'woogles.io', 'content-type': 'application/json', origin: 'https://woogles.io' };
+    const body = { lexicon: lexicon, words: alphagrams.split(/[\s,]+/), definitions: true, anagrams: true };
+    let status, statusText;
+    return fetch(url, { method: 'POST', body: JSON.stringify(body), headers: headers })
+        .then(response => { status = response.status; statusText = response.statusText; return response.json(); })
+        .then(json => formatPages('Alphagram', Object.entries(json.results).map(entry => formatEntry(lexicon, ...entry)), interaction, 'Alphagrams not found!'))
         .catch(error => {
-            console.log(`Error in anagram(${alphagrams}): \
-                ${error.response.status} ${error.response.statusText}`);
-            return `An error occurred handling your request: \
-                ${error.response.status} ${error.response.statusText}`;
+            console.log(`Error in anagram(${lexicon}, ${alphagrams}): ${error}`);
+            return `An error occurred handling your request: ${status} ${statusText}`;
         });
 }
 
@@ -34,12 +33,12 @@ const flags = {
 
 async function process(bot, msg, suffix) {
     const [lexicon, alphagrams] = suffix.toUpperCase().split(/[^A-Z0-9?]+/i, 2);
-    if (alphagrams.includes('?'))
+    if (alphagrams && alphagrams.includes('?'))
         await msg.channel.send('Blank tiles are not supported');
     else if (lexicon && formatLexicon(lexicon) && alphagrams)
         await anagram(lexicon, alphagrams).then(message => msg.channel.send(message));
     else
-        await msg.channel.send(formatLexicon(lexicon) ? 'Words not specified!' : 'Lexicon not found!');
+        await msg.channel.send(formatLexicon(lexicon) ? 'Lexicon or words not specified!' : 'Lexicon not found!');
 }
 
 async function interact(interaction) {
