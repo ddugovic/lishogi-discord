2b5a96d94c8cccb73f92d268597924d3abfa11b3
Daniel Dugovic <ddugovic@users.noreply.github.com>
2023-10-06 11:41:09 -0500

    Rename parameter to local_hour for clarity

--- commands/timestamp.js
+++ commands/timestamp.js
@@ -11,7 +11,7 @@ function process(bot, msg, suffix) {
 
 async function interact(interaction) {
     await interaction.deferReply();
-    return timestamp(interaction.options.getInteger('year'), interaction.options.getInteger('month'), interaction.options.getInteger('day'), interaction.options.getInteger('gmt_hour'), interaction.options.getInteger('minute'), interaction.options.getInteger('offset'), interaction.options.getInteger('offsetmin'));
+    return timestamp(interaction.options.getInteger('year'), interaction.options.getInteger('month'), interaction.options.getInteger('day'), interaction.options.getInteger('local_hour'), interaction.options.getInteger('minute'), interaction.options.getInteger('local_offset'), interaction.options.getInteger('local_offsetmin'));
 }
 
 module.exports = { process, interact };
