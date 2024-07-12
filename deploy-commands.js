c22a9df6878d19966294c3086ae0359080a0205c
Daniel Dugovic <ddugovic@users.noreply.github.com>
2024-07-12 04:28:48 -0500

    Code cleanup

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,98 +1,98 @@
-const { PermissionFlagsBits } = require('discord-api-types/v10');
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { PermissionFlagsBits } = require("discord-api-types/v10");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 const arenas = [
-    { name: 'Annan shogi', value: 'annanshogi' },
-    { name: 'Blitz', value: 'blitz' },
-    { name: 'Bullet', value: 'bullet' },
-    { name: 'Checkshogi', value: 'checkshogi' },
-    { name: 'Chu shogi', value: 'chushogi' },
-    { name: 'Classical', value: 'classical' },
-    { name: 'Kyoto shogi', value: 'kyotoshogi' },
-    { name: 'Minishogi', value: 'minishogi' },
-    { name: 'Rapid', value: 'rapid' },
-    { name: 'Thematic', value: 'thematic' },
-    { name: 'UltraBullet', value: 'ultraBullet' }
+    { name: "Annan shogi", value: "annanshogi" },
+    { name: "Blitz", value: "blitz" },
+    { name: "Bullet", value: "bullet" },
+    { name: "Checkshogi", value: "checkshogi" },
+    { name: "Chu shogi", value: "chushogi" },
+    { name: "Classical", value: "classical" },
+    { name: "Kyoto shogi", value: "kyotoshogi" },
+    { name: "Minishogi", value: "minishogi" },
+    { name: "Rapid", value: "rapid" },
+    { name: "Thematic", value: "thematic" },
+    { name: "UltraBullet", value: "ultraBullet" }
 ];
 const categories = [
-    { name: 'Players', value: 'Players' },
-    { name: 'Proverbs', value: 'Proverbs' },
-    { name: 'Strategies', value: 'Strategies' },
-    { name: 'Terminology', value: 'Terminology' }
+    { name: "Players", value: "Players" },
+    { name: "Proverbs", value: "Proverbs" },
+    { name: "Strategies", value: "Strategies" },
+    { name: "Terminology", value: "Terminology" }
 ];
 const channels = [
-    { name: 'Standard', value: 'standard' },
-    { name: 'Annan shogi', value: 'annanshogi' },
-    { name: 'Checkshogi', value: 'checkshogi' },
-    { name: 'Chu shogi', value: 'chushogi' },
-    { name: 'Kyoto shogi', value: 'kyotoshogi' },
-    { name: 'Minishogi', value: 'minishogi' },
-    { name: 'Computer', value: 'computer' }
+    { name: "Standard", value: "standard" },
+    { name: "Annan shogi", value: "annanshogi" },
+    { name: "Checkshogi", value: "checkshogi" },
+    { name: "Chu shogi", value: "chushogi" },
+    { name: "Kyoto shogi", value: "kyotoshogi" },
+    { name: "Minishogi", value: "minishogi" },
+    { name: "Computer", value: "computer" }
 ];
 const modes = [
-    { name: 'Annan shogi', value: 'annanshogi' },
-    { name: 'Blitz', value: 'blitz' },
-    { name: 'Bullet', value: 'bullet' },
-    { name: 'Checkshogi', value: 'checkshogi' },
-    { name: 'Chu shogi', value: 'chushogi' },
-    { name: 'Classical', value: 'classical' },
-    { name: 'Kyoto shogi', value: 'kyotoshogi' },
-    { name: 'Minishogi', value: 'minishogi' },
-    { name: 'Puzzle', value: 'puzzle' },
-    { name: 'Rapid', value: 'rapid' },
-    { name: 'UltraBullet', value: 'ultraBullet' }
+    { name: "Annan shogi", value: "annanshogi" },
+    { name: "Blitz", value: "blitz" },
+    { name: "Bullet", value: "bullet" },
+    { name: "Checkshogi", value: "checkshogi" },
+    { name: "Chu shogi", value: "chushogi" },
+    { name: "Classical", value: "classical" },
+    { name: "Kyoto shogi", value: "kyotoshogi" },
+    { name: "Minishogi", value: "minishogi" },
+    { name: "Puzzle", value: "puzzle" },
+    { name: "Rapid", value: "rapid" },
+    { name: "UltraBullet", value: "ultraBullet" }
 ];
 const ratings = [
-    { name: 'Annan shogi', value: 'Annanshogi' },
-    { name: 'Blitz', value: 'blitz' },
-    { name: 'Bullet', value: 'bullet' },
-    { name: 'Checkshogi', value: 'checkshogi' },
-    { name: 'Chu shogi', value: 'chushogi' },
-    { name: 'Classical', value: 'classical' },
-    { name: 'Correspondence', value: 'correspondence' },
-    { name: 'Kyoto shogi', value: 'kyotoshogi' },
-    { name: 'Minishogi', value: 'minishogi' },
-    { name: 'Puzzle', value: 'puzzle' },
-    { name: 'Rapid', value: 'rapid' },
-    { name: 'UltraBullet', value: 'ultraBullet' }
+    { name: "Annan shogi", value: "Annanshogi" },
+    { name: "Blitz", value: "blitz" },
+    { name: "Bullet", value: "bullet" },
+    { name: "Checkshogi", value: "checkshogi" },
+    { name: "Chu shogi", value: "chushogi" },
+    { name: "Classical", value: "classical" },
+    { name: "Correspondence", value: "correspondence" },
+    { name: "Kyoto shogi", value: "kyotoshogi" },
+    { name: "Minishogi", value: "minishogi" },
+    { name: "Puzzle", value: "puzzle" },
+    { name: "Rapid", value: "rapid" },
+    { name: "UltraBullet", value: "ultraBullet" }
 ];
 const statuses = [
-    { name: 'Created', value: 'created' },
-    { name: 'Started', value: 'started' },
-    { name: 'Finished', value: 'finished' }
+    { name: "Created", value: "created" },
+    { name: "Started", value: "started" },
+    { name: "Finished", value: "finished" }
 ];
 
 const guildCommands = [
-    new SlashCommandBuilder().setName('stop').setDescription("Stop Lishogi Statbot (owner only)")
+    new SlashCommandBuilder().setName("stop").setDescription("Stop Lishogi Statbot (owner only)")
 ]
     .map(command => command.setDefaultMemberPermissions(PermissionFlagsBits.Administrator).toJSON());
 
 const commands = [
-    new SlashCommandBuilder().setName('arena').setDescription("Find a created, started, or finished arena").addStringOption(option => option.setName('mode').setDescription('Select a game mode').addChoices(...arenas)).addStringOption(option => option.setName('status').setDescription('Select an arena status').addChoices(...statuses)),
-    new SlashCommandBuilder().setName('blog').setDescription("Display recent blog entries"),
-    new SlashCommandBuilder().setName('bots').setDescription("Display online bots"),
-    new SlashCommandBuilder().setName('broadcast').setDescription("Find an upcoming or recent broadcast created by lishogi"),
-    new SlashCommandBuilder().setName('coach').setDescription("Find a coach"),
-    new SlashCommandBuilder().setName('deleteuser').setDescription("Delete your lishogi username from the bot's database"),
-    new SlashCommandBuilder().setName('eval').setDescription("Get the cached evaluation of a position, if available").addStringOption(option => option.setName('sfen').setDescription('SFEN (Shogi Forsyth-Edwards Notation)')),
-    new SlashCommandBuilder().setName('fesa').setDescription("Display FESA news"),
-    new SlashCommandBuilder().setName('leaderboard').setDescription("Display top-rated players").addStringOption(option => option.setName('mode').setDescription('Select a game mode').addChoices(...modes)),
-    new SlashCommandBuilder().setName('playing').setDescription("Share your (or a user's) current game").addStringOption(option => option.setName('username').setDescription('Enter lishogi player username')),
-    new SlashCommandBuilder().setName('news').setDescription("Display recent news"),
-    new SlashCommandBuilder().setName('privacy').setDescription("View privacy policy"),
-    new SlashCommandBuilder().setName('profile').setDescription("Display your (or a user's) profile").addStringOption(option => option.setName('username').setDescription('Enter lishogi player username')),
-    new SlashCommandBuilder().setName('puzzle').setDescription("Display today's puzzle"),
-    new SlashCommandBuilder().setName('schedule').setDescription("Schedule event (tournament) game (see timestamp command)").addStringOption(option => option.setName('event').setDescription('Event (tournament) name').setRequired(true)).addUserOption(option => option.setName('sente').setDescription('Sente (first player)').setRequired(true)).addUserOption(option => option.setName('gote').setDescription('Gote (second player)').setRequired(true)).addIntegerOption(option => option.setName('year').setDescription('Year (2000-2100)').setMinValue(2000).setMaxValue(2100).setRequired(true)).addIntegerOption(option => option.setName('month').setDescription('Month (1-12)').setMinValue(1).setMaxValue(12).setRequired(true)).addIntegerOption(option => option.setName('day').setDescription('Day (1-31)').setMinValue(1).setMaxValue(31).setRequired(true)).addIntegerOption(option => option.setName('utc_hour').setDescription('UTC hour (0-23)').setMinValue(0).setMaxValue(23).setRequired(true)).addIntegerOption(option => option.setName('minute').setDescription('Minute (0-59)').setMinValue(0).setMaxValue(59).setRequired(true)),
-    new SlashCommandBuilder().setName('setgamemode').setDescription("Set your favorite game (or puzzle) mode").addStringOption(option => option.setName('mode').setDescription('Enter your favorite game (or puzzle) mode').addChoices(...ratings)),
-    new SlashCommandBuilder().setName('setuser').setDescription("Set your lishogi username").addStringOption(option => option.setName('username').setDescription('Enter your lishogi username').setAutocomplete(true).setMaxLength(30).setMinLength(2).setRequired(true)),
-    new SlashCommandBuilder().setName('simul').setDescription("Display a recently finished, ongoing, or upcoming simultanous exhibition"),
-    new SlashCommandBuilder().setName('streamers').setDescription("Display live streamers"),
-    new SlashCommandBuilder().setName('team').setDescription("Search teams for a keyword").addStringOption(option => option.setName('text').setDescription('Search keywords').setRequired(true)),
-    new SlashCommandBuilder().setName('timestamp').setDescription("Print discord magic timestamp").addIntegerOption(option => option.setName('year').setDescription('Year (2000-2100)').setMinValue(2000).setMaxValue(2100).setRequired(true)).addIntegerOption(option => option.setName('month').setDescription('Month (1-12)').setMinValue(1).setMaxValue(12).setRequired(true)).addIntegerOption(option => option.setName('day').setDescription('Day (1-31)').setMinValue(1).setMaxValue(31).setRequired(true)).addIntegerOption(option => option.setName('utc_hour').setDescription('UTC hour (0-23)').setMinValue(0).setMaxValue(23).setRequired(true)).addIntegerOption(option => option.setName('minute').setDescription('Minute (0-59)').setMinValue(0).setMaxValue(59).setRequired(true)),
-    new SlashCommandBuilder().setName('tv').setDescription("Display TV game list").addStringOption(option => option.setName('mode').setDescription('Select channel').addChoices(...channels)),
-    new SlashCommandBuilder().setName('video').setDescription("Search videos for a keyword").addStringOption(option => option.setName('text').setDescription('Search keywords')),
-    new SlashCommandBuilder().setName('wiki').setDescription("Display Shogi Harbour wiki pages").addStringOption(option => option.setName('category').setDescription('Category').setRequired(true).addChoices(...categories)),
-    new SlashCommandBuilder().setName('help').setDescription("Display a list of available commands")
+    new SlashCommandBuilder().setName("arena").setDescription("Find a created, started, or finished arena").addStringOption(option => option.setName("mode").setDescription("Select a game mode").addChoices(...arenas)).addStringOption(option => option.setName("status").setDescription("Select an arena status").addChoices(...statuses)),
+    new SlashCommandBuilder().setName("blog").setDescription("Display recent blog entries"),
+    new SlashCommandBuilder().setName("bots").setDescription("Display online bots"),
+    new SlashCommandBuilder().setName("broadcast").setDescription("Find an upcoming or recent broadcast created by lishogi"),
+    new SlashCommandBuilder().setName("coach").setDescription("Find a coach"),
+    new SlashCommandBuilder().setName("deleteuser").setDescription("Delete your lishogi username from the bot"s database"),
+    new SlashCommandBuilder().setName("eval").setDescription("Get the cached evaluation of a position, if available").addStringOption(option => option.setName("sfen").setDescription("SFEN (Shogi Forsyth-Edwards Notation)")),
+    new SlashCommandBuilder().setName("fesa").setDescription("Display FESA news"),
+    new SlashCommandBuilder().setName("leaderboard").setDescription("Display top-rated players").addStringOption(option => option.setName("mode").setDescription("Select a game mode").addChoices(...modes)),
+    new SlashCommandBuilder().setName("playing").setDescription("Share your (or a user's) current game").addStringOption(option => option.setName("username").setDescription("Enter lishogi player username")),
+    new SlashCommandBuilder().setName("news").setDescription("Display recent news"),
+    new SlashCommandBuilder().setName("privacy").setDescription("View privacy policy"),
+    new SlashCommandBuilder().setName("profile").setDescription("Display your (or a user's) profile").addStringOption(option => option.setName("username").setDescription("Enter lishogi player username")),
+    new SlashCommandBuilder().setName("puzzle").setDescription("Display today"s puzzle"),
+    new SlashCommandBuilder().setName("schedule").setDescription("Schedule event (tournament) game (see timestamp command)").addStringOption(option => option.setName("event").setDescription("Event (tournament) name").setRequired(true)).addUserOption(option => option.setName("sente").setDescription("Sente (first player)").setRequired(true)).addUserOption(option => option.setName("gote").setDescription("Gote (second player)").setRequired(true)).addIntegerOption(option => option.setName("year").setDescription("Year (2000-2100)").setMinValue(2000).setMaxValue(2100).setRequired(true)).addIntegerOption(option => option.setName("month").setDescription("Month (1-12)").setMinValue(1).setMaxValue(12).setRequired(true)).addIntegerOption(option => option.setName("day").setDescription("Day (1-31)").setMinValue(1).setMaxValue(31).setRequired(true)).addIntegerOption(option => option.setName("utc_hour").setDescription("UTC hour (0-23)").setMinValue(0).setMaxValue(23).setRequired(true)).addIntegerOption(option => option.setName("minute").setDescription("Minute (0-59)").setMinValue(0).setMaxValue(59).setRequired(true)),
+    new SlashCommandBuilder().setName("setgamemode").setDescription("Set your favorite game (or puzzle) mode").addStringOption(option => option.setName("mode").setDescription("Enter your favorite game (or puzzle) mode").addChoices(...ratings)),
+    new SlashCommandBuilder().setName("setuser").setDescription("Set your lishogi username").addStringOption(option => option.setName("username").setDescription("Enter your lishogi username").setAutocomplete(true).setMaxLength(30).setMinLength(2).setRequired(true)),
+    new SlashCommandBuilder().setName("simul").setDescription("Display a recently finished, ongoing, or upcoming simultanous exhibition"),
+    new SlashCommandBuilder().setName("streamers").setDescription("Display live streamers"),
+    new SlashCommandBuilder().setName("team").setDescription("Search teams for a keyword").addStringOption(option => option.setName("text").setDescription("Search keywords").setRequired(true)),
+    new SlashCommandBuilder().setName("timestamp").setDescription("Print discord magic timestamp").addIntegerOption(option => option.setName("year").setDescription("Year (2000-2100)").setMinValue(2000).setMaxValue(2100).setRequired(true)).addIntegerOption(option => option.setName("month").setDescription("Month (1-12)").setMinValue(1).setMaxValue(12).setRequired(true)).addIntegerOption(option => option.setName("day").setDescription("Day (1-31)").setMinValue(1).setMaxValue(31).setRequired(true)).addIntegerOption(option => option.setName("utc_hour").setDescription("UTC hour (0-23)").setMinValue(0).setMaxValue(23).setRequired(true)).addIntegerOption(option => option.setName("minute").setDescription("Minute (0-59)").setMinValue(0).setMaxValue(59).setRequired(true)),
+    new SlashCommandBuilder().setName("tv").setDescription("Display TV game list").addStringOption(option => option.setName("mode").setDescription("Select channel").addChoices(...channels)),
+    new SlashCommandBuilder().setName("video").setDescription("Search videos for a keyword").addStringOption(option => option.setName("text").setDescription("Search keywords")),
+    new SlashCommandBuilder().setName("wiki").setDescription("Display Shogi Harbour wiki pages").addStringOption(option => option.setName("category").setDescription("Category").setRequired(true).addChoices(...categories)),
+    new SlashCommandBuilder().setName("help").setDescription("Display a list of available commands")
 ]
     .map(command => command.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages).toJSON());
 
