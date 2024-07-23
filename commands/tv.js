ac9dadcda2b412dd5b3c03bcb303b1c1d845c984
Daniel Dugovic <ddugovic@users.noreply.github.com>
2024-07-23 08:39:31 -0500

    Format TV live game moves

--- commands/tv.js
+++ commands/tv.js
@@ -1,8 +1,9 @@
 const { EmbedBuilder } = require('discord.js');
+const plural = require('plural');
 const formatClock = require('../lib/format-clock');
 const formatColor = require('../lib/format-color');
 const { formatHandicap, formatVariant } = require('../lib/format-variant');
-const { formatSanVariation, numberVariation } = require('../lib/format-variation');
+const { formatOpening } = require('../lib/format-variation');
 const parseDocument = require('../lib/parse-document');
 const User = require('../models/User');
 
@@ -29,36 +30,45 @@ async function getMode(author) {
         return user.favoriteMode;
 }
 
-function formatChannel(mode, name, channel) {
-    const embed = new EmbedBuilder()
+async function formatChannel(mode, name, channel) {
+    var embed = new EmbedBuilder()
         .setColor(getColor(channel))
         .setThumbnail(`https://lishogi1.org/game/export/gif/thumbnail/${channel.gameId}.gif`)
         .setTitle(`${name} :tv: ${formatPlayer(channel)}`)
         .setURL(`https://lishogi.org/tv/${mode}`)
         .setDescription(`Sit back, relax, and watch the best ${name} games on Lishogi!`);
 
-    return getLiveGames(mode)
-        .then(games => games.length == 0 ? embed : embed.addFields({ name: 'Live Games', value: games.map(formatGame).join('\n') }));
+    const games = await getLiveGames(mode);
+    if (games.length) {
+        const fields = await Promise.all(games.filter(game => game.status != 'aborted').map(formatGame));
+        embed = embed.addFields({ name: `Live ${plural('Game', fields.length)}`, value: fields.join('\n') });
+    }
+    return embed;
 }
 
-async function getLiveGames(channel) {
+function getLiveGames(channel) {
     const url = `https://lishogi.org/api/tv/${channel ?? 'best'}?clocks=false&nb=3`;
     return fetch(url, { headers: { Accept: 'application/x-ndjson' }, params: { nb: 3 } })
         .then(response => response.text())
         .then(text => parseDocument(text));
 }
 
-function formatGame(game) {
-    const url = `https://lishogi.org/${game.id}`;
+async function formatGame(game) {
+    const winner = game.winner ? game.players[`${game.winner}`].user : undefined;
+    const outcome = winner && winner.name == username ? ':white_circle:' : game.winner ? ':black_circle:' : ':hourglass:';
     const players = [game.players.sente, game.players.gote].map(formatPlayer).join(' - ');
-    const opening = game.moves ? `\n${formatOpening(game.opening, game.moves)}` : '';
-    return `${formatClock(game.clock, game.daysPerTurn)} [${players}](${url})${opening}`;
+    const url = `https://lishogi.org/${game.id}`;
+    const status = formatStatus(game);
+    const opening = game.moves ? `${await formatOpening(game.variant, game.opening, game.initialSfen, game.moves)}` : '';
+    return `${outcome} ${formatClock(game.clock, game.daysPerTurn)} ${status[0]} [${players}](${url}) ${status[1]} (${formatHandicap(game.variant, game.initialSfen)}) <t:${Math.floor(game.createdAt / 1000)}:R>${opening}`;
 }
 
-function formatOpening(opening, moves) {
-    const ply = opening ? opening.ply : 10;
-    const variation = moves.split(/ /).slice(0, ply);
-    return opening ? `${opening.name} *${formatSanVariation(null, variation)}*` : `*${numberVariation(variation)}*`;
+function formatRatingDiff(ratingDiff) {
+    return (ratingDiff > 0) ? ` ▲**${ratingDiff}**` : (ratingDiff < 0) ? ` ▼**${Math.abs(ratingDiff)}**` : '';
+}
+
+function formatStatus(game) {
+    return [game.players.sente.ratingDiff, game.players.gote.ratingDiff].map(formatRatingDiff);
 }
 
 function getColor(game) {
