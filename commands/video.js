8a15c2d6bad68bef5dd393dc9ce779641aac908c
Daniel Dugovic <ddugovic@users.noreply.github.com>
2026-04-20 21:33:11 -0500

    Fix video search (do not expect duration)

--- commands/video.js
+++ commands/video.js
@@ -20,20 +20,20 @@ function video(author, text, interaction) {
 
 function getVideos(document) {
     const videos = [];
-    const pattern = /<a class="[ \w]+" href="\/video\/([-\w]+)?\??(?:q=[ \w]+)?"><span class="duration">(.+?)<\/span>.+?<span class="full-title">(.+?)<\/span><span class="author">(.+?)<\/span><span class="target">(.+?)<\/span>/g;
+    const pattern = /<a class="[ \w]+" href="\/video\/([-\w]+)?\??(?:q=[ \w]+)?">.+?<span class="full-title">(.+?)<\/span><span class="author">(.+?)<\/span><span class="target"><div class="target__box">(.+?)<\/div><\/span>/g;
     for (match of document.matchAll(pattern))
-        videos.push([match[1], match[2], match[3], match[4], match[5]]);
+        videos.push([match[1], match[2], match[3], match[4]]);
     return videos;
 }
 
 function formatVideo(video) {
-    const [uri, duration, name, author, target] = video;
-    const seconds = duration.split(/:/).reduce((acc,time) => (60 * acc) + +time);
+    const [uri, name, author, target] = video;
+    const seconds = 3 * name.length;
     const score = Math.min(Math.max(Math.floor(2 * Math.sqrt(seconds)), 0), 255);
     return new EmbedBuilder()
         .setColor(formatColor(score, 0, 255-score))
         .setAuthor({name: author, iconURL: null})
-        .setTitle(`${decode(name)} (${duration})`)
+        .setTitle(decode(name))
         .setURL(`https://youtu.be/${uri}`)
         .setThumbnail(`https://img.youtube.com/vi/${uri}/0.jpg`)
         .addFields({ name: 'Target', value: title(target), inline: true });
